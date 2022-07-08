98903c2eea6a9cd06be3a1f3cde2b73521b101f6
John <jpilik@fream.pl>
2022-07-08 15:25:43 +0200

    turning fcards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 import { Flashcard } from "./Flashcard/Flashcard";
 
 function App() {
+  const [fcards, setfcards] = useState([
+    {
+      unturned: "Example fcard",
+      turned: "Not a fuck you card",
+    },
+  ]);
+
+  function addFcard() {
+    // setfcards(...fcards, fcard);
+  }
+
   return (
     <div className="App">
       <header className="App-header">header</header>
       <main>
-        <Flashcard />
+        {fcards.map((fcard) => {
+          return <Flashcard fcard={fcard} />;
+        })}
+        <form onSubmit={() => addFcard}>
+          <input type="submit" value="Add flashcard" />
+        </form>
       </main>
       <footer>footer</footer>
     </div>
