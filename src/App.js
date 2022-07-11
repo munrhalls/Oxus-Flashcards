8716ba649a89fbc607275e7a6d911f0addc28cc2
John <jpilik@fream.pl>
2022-07-11 15:33:51 +0200

    firebase works

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import "./App.css";
 import { useState } from "react";
 import { Flashcard } from "./Flashcard/Flashcard";
 import { AddFcard } from "./Flashcard/AddFCard";
-import ScreenCapture from "./Screener/ScreenCapture";
-import Screener from "./Screener/Screener";
+import { db } from "./Firebase/app";
 
 function App() {
   const [fcards, setfcards] = useState([
@@ -21,15 +20,13 @@ function App() {
     },
   ]);
 
+  console.log(db);
   function onAddFcard(fcard) {
-    console.log(fcard);
     setfcards(() => [...fcards, fcard]);
   }
-
   return (
     <div className="App">
       <header className="Header">Flashcards</header>
-      <Screener />
       <main className="Main">
         <div className="Flashcards">
           {fcards.map((fcard) => {
