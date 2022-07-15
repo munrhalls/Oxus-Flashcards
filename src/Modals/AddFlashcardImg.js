87800c75411fc81250ff122088a3a0895a5c99b0
John <jpilik@fream.pl>
2022-07-15 15:00:49 +0200

    comp addimg

---
+++ src/Modals/AddFlashcardImg.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export const AddFlashcardImg = ({ src }) => {
+  return src ? (
+    <img
+      className="Flashcard__form__frameImage"
+      src={src}
+      alt="Add flashcard image. Edit here."
+    />
+  ) : (
+    <div className="Flashcard__form__frameNoImage">
+      <span className="Flashcard__form__frameNoImageTitle">
+        Want image in flashcard?
+      </span>
+      <span className="Flashcard__form__frameNoImageTitle">
+        Click "choose file" and select an image from your computer.
+      </span>
+      <span className="Flashcard__form__frameNoImageTitle">
+        You can edit it here.
+      </span>
+    </div>
+  );
+};
