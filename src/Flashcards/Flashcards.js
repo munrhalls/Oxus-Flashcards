ec4e1d78e1988d6dcc8d3bf550f3457388182649
John <antarcticdepths@gmail.com>
2022-07-18 17:59:08 +0200

    big flashcard

--- src/Flashcards/Flashcards.js
+++ src/Flashcards/Flashcards.js
@@ -1,12 +1,31 @@
+import { uuidv4 } from "@firebase/util";
 import React, { useState } from "react";
 import { Flashcard } from "./Flashcard";
 
 export const Flashcards = ({ flashcards }) => {
+  const [current, setCurrent] = useState(1);
+  function showNext() {
+    if (flashcards.length > current) {
+      setCurrent(() => current + 1);
+    } else {
+      setCurrent(1);
+    }
+  }
   return (
     <div className="Flashcards">
       {flashcards.map((flashcard) => {
-        return <Flashcard key={Math.random()} flashcard={flashcard} />;
+        return flashcard.order === current ? (
+          <div key={uuidv4()}>
+            <div className="Flashcard__number">{flashcard.order}</div>
+            <Flashcard key={uuidv4()} flashcard={flashcard} />
+          </div>
+        ) : (
+          ""
+        );
       })}
+      <button key={uuidv4()} onClick={showNext}>
+        Next
+      </button>
     </div>
   );
 };
