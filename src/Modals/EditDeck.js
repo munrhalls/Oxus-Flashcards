60837ed58eba936279b93e2601898b3959749d1d
John <antarcticdepths@gmail.com>
2022-07-23 15:08:03 +0200

    reorg

--- src/Modals/EditDeck.js
+++ src/Modals/EditDeck.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { Deck } from "../Decks/Deck";
+import { Flashcard } from "../Flashcards/Flashcard";
 import { AddFlashcard } from "./AddFlashcard";
+import { uuidv4 } from "@firebase/util";
 
 export const EditDeck = ({ closeModal, activeDeck }) => {
   console.log(activeDeck);
@@ -9,8 +12,12 @@ export const EditDeck = ({ closeModal, activeDeck }) => {
         <>
           {/* <AddFlashcard /> */}
           <h1 className="EditDeck__deckName">{activeDeck.name}</h1>
-
-          <button className="EditDeck__btn__close" onClick={() => closeModal()}>CLOSE</button>
+          {activeDeck.flashcards.map((card) => {
+            return <Flashcard key={uuidv4()} flashcard={card} />;
+          })}
+          <button className="EditDeck__btn__close" onClick={() => closeModal()}>
+            CLOSE
+          </button>
         </>
       ) : (
         <div className="EditDeck__noDeckSelectedMsg">
