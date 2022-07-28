912193dcf553e07f9b50ed608da2f5529e784b77
John <jpilik@fream.pl>
2022-07-28 18:26:35 +0200

    refactor

--- src/Decks/Deck.js
+++ src/Decks/Deck.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Flashcards } from "../Flashcards/Flashcards";
 import { CompletedFlashcards } from "../Flashcards/CompletedFlashcards";
 
-export const Deck = ({
-  flashcards,
-  setFlashcards,
-  completedFlashcards,
-  setCompletedFlashcards,
-}) => {
+export const Deck = ({ activeDeck, decks, setDecks }) => {
+  const deck = decks.filter((deck) => deck.id === activeDeck.id)[0];
+  const [flashcards, setFlashcards] = useState(deck.flashcards);
+  const [completedFlashcards, setCompletedFlashcards] = useState(
+    deck.completedFlashcards
+  );
+
   return (
     <div className="Deck">
       <h3 className="Deck__title">INITIAL DECK TITLE</h3>
       <div className="Deck__decks">
         <Flashcards
           flashcards={flashcards}
-          setFlashcards={setFlashcards}
+          setFlashcards={(flashcards) => setFlashcards(flashcards)}
           completedFlashcards={completedFlashcards}
-          setCompletedFlashcards={setCompletedFlashcards}
+          setCompletedFlashcards={(completedFlashcards) =>
+            setCompletedFlashcards(completedFlashcards)
+          }
         />
         <CompletedFlashcards completedFlashcards={completedFlashcards} />
       </div>
