e0e338f36abb4af80800867c8860c62eadd0fc15
John <antarcticdepths@gmail.com>
2022-07-29 15:57:54 +0200

    FIX DATA MODEL 2

--- src/Decks/Deck.js
+++ src/Decks/Deck.js
@@ -3,14 +3,20 @@ import { Flashcards } from "../Flashcards/Flashcards";
 import { CompletedFlashcards } from "../Flashcards/CompletedFlashcards";
 import cloneDeep from "lodash.clonedeep";
 
-export const Deck = ({ activeDeck, decks, updateDeckFlashcards }) => {
+export const Deck = ({
+  activeDeck,
+  updateDeckFlashcards,
+  updateDeckCompletedFlashcards,
+}) => {
   return (
     <div className="Deck">
       <h3 className="Deck__title">DECK: {activeDeck.name}.</h3>
       <div className="Deck__decks">
         <Flashcards
           flashcards={activeDeck.flashcards}
+          completedFlashcards={activeDeck.completedFlashcards}
           updateDeckFlashcards={updateDeckFlashcards}
+          updateDeckCompletedFlashcards={updateDeckCompletedFlashcards}
         />
         <CompletedFlashcards
           completedFlashcards={activeDeck.completedFlashcards}
