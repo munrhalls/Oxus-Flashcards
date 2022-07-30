38137f554dc51841146b2e9db4d840e7a0e809f5
John <jpilik@fream.pl>
2022-07-30 20:10:15 +0200

    refactor, more readable names

--- src/Modals/AddFlashcard/UnturnedFlashcard.js
+++ src/Modals/AddFlashcard/UnturnedFlashcard.js
@@ -21,19 +21,17 @@ export const UnturnedFlashcard = ({
   return (
     <div className="Flashcard_form_cell">
       <div className="Flashcard_form_label">UNTURNED</div>
-      <div className="Form__manageflashcard__inputContainer">
-        <span className="Form__manageflashcard__inputTitle">Add text</span>
+      <div className="Form__flashcard__inputContainer">
+        <span className="Form__flashcard__inputTitle">Add text</span>
         <textarea
-          className="Form__manageflashcard__textArea"
+          className="Form__flashcard__textArea"
           placeholder="Type..."
           value={flashcard.unturned.text}
           onChange={changeUnturnedText}
         />
-        <span className="Form__manageflashcard__inputTitle --image">
-          Add image
-        </span>
+        <span className="Form__flashcard__inputTitle --image">Add image</span>
         <label
-          className="Form__manageflashcard__imageInput"
+          className="Form__flashcard__imageInput"
           htmlFor="changeUnturnedImg"
         >
           UPLOAD IMAGE
