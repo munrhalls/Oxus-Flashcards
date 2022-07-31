befb59319375a2d34ad8b24661887256198c772a
John <jpilik@fream.pl>
2022-07-31 16:57:29 +0200

    fix problem

--- src/App.js
+++ src/App.js
@@ -20,129 +20,11 @@ function App() {
     {
       id: uuidv4(),
       unturned: {
-        text: "Question 1",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "Answer 2",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 3",
+        text: "Example flashcard. 2 + 2 = ?",
         image: "",
       },
       turned: {
-        text: "Answer is 33333333333333. 3..",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 4",
-        image: "",
-      },
-      turned: {
-        text: "That's answer 44444444444444. Four. Actually.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question",
-        image: "",
-      },
-      turned: {
-        text: "555555555555555555555 55 4 5 5 5 5",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "Six.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 7",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "Seven.Seven. Seven.Seven.Seven.Seven.Seven.Seven.Seven. Seven.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 8",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "That's 8.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 9",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "That's nine. 9..... 9.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 10",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "Ten ten ten.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 11",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "11n.",
-        image: "",
-      },
-    },
-    {
-      id: uuidv4(),
-      unturned: {
-        text: "Question 12",
-        image:
-          "https://firebasestorage.googleapis.com/v0/b/oxus-9ce02.appspot.com/o/flashcards?alt=media&token=5dcb00f9-6961-432e-aa2e-0fef14c259c4",
-      },
-      turned: {
-        text: "12.",
+        text: "9000.",
         image: "",
       },
     },
@@ -154,15 +36,13 @@ function App() {
 
   function getDecks() {
     let decks = [];
-    for (let i = 0; i < 10; i++) {
-      let deck = {
-        id: uuidv4(),
-        name: i,
-        flashcards: mock,
-        completedFlashcards: [],
-      };
-      decks.push(deck);
-    }
+    let deck = {
+      id: uuidv4(),
+      name: "Example deck.",
+      flashcards: mock,
+      completedFlashcards: [],
+    };
+    decks.push(deck);
     return decks;
   }
   async function fileTest(e) {
