ee38793e389a166dca31bf36d90c1a86ce0bdb58
John <antarcticdepths@gmail.com>
2022-08-02 12:34:54 +0200

    FIX DATA MODEL

--- src/Decks/Deck.js
+++ src/Decks/Deck.js
@@ -3,28 +3,18 @@ import { Flashcards } from "../Flashcards/Flashcards";
 import { CompletedFlashcards } from "../Flashcards/CompletedFlashcards";
 import cloneDeep from "lodash.clonedeep";
 
-export const Deck = ({ activeDeckId, decks, setDecks }) => {
-  const deck = decks.filter((deck) => deck.id === activeDeckId)[0];
-  const [flashcards, setFlashcards] = useState(() => deck.flashcards);
-  const [completedFlashcards, setCompletedFlashcards] = useState(
-    () => deck.completedFlashcards
-  );
-  deck.flashcards = flashcards;
-  deck.completedFlashcards = completedFlashcards;
-
+export const Deck = ({ activeDeck, decks, updateDeckFlashcards }) => {
   return (
     <div className="Deck">
-      <h3 className="Deck__title">DECK: {deck.name}.</h3>
+      <h3 className="Deck__title">DECK: {activeDeck.name}.</h3>
       <div className="Deck__decks">
         <Flashcards
-          flashcards={flashcards}
-          setFlashcards={(flashcards) => setFlashcards(flashcards)}
-          completedFlashcards={completedFlashcards}
-          setCompletedFlashcards={(completedFlashcards) =>
-            setCompletedFlashcards(completedFlashcards)
-          }
+          flashcards={activeDeck.flashcards}
+          updateDeckFlashcards={updateDeckFlashcards}
         />
-        <CompletedFlashcards completedFlashcards={completedFlashcards} />
+        <CompletedFlashcards
+          completedFlashcards={activeDeck.completedFlashcards}
+        />
       </div>
     </div>
   );
--- src/Flashcards/Flashcards.js
+++ src/Flashcards/Flashcards.js
@@ -5,61 +5,53 @@ import img from "./../Assets/right-long-black-arrow.png";
 import { dblClick } from "@testing-library/user-event/dist/click";
 import cloneDeep from "lodash.clonedeep";
 
-export const Flashcards = ({
-  flashcards,
-  setFlashcards,
-  setCompletedFlashcards,
-  completedFlashcards,
-}) => {
+export const Flashcards = ({ flashcards, updateDeckFlashcards }) => {
   const [difficulty, setDifficulty] = useState(3);
   const levels = ["pass", "easy", "medium", "hard"];
+
   function shuffleHard(flashcards) {
     const wasFirst = flashcards.shift();
     let moveBy = Math.floor(flashcards.length / 3);
     let randomizer = randomIntFromInterval(-2, 0);
-    if (flashcards.length > 4) {
-      moveBy += randomizer;
-    }
+    if (flashcards.length > 4) moveBy += randomizer;
+
     flashcards.splice(moveBy, 0, wasFirst);
   }
   function shuffleMedium(flashcards) {
     const wasFirst = flashcards.shift();
     let moveBy = Math.floor(flashcards.length / 2);
     let randomizer = randomIntFromInterval(-2, 2);
-    if (flashcards.length > 4) {
-      moveBy += randomizer;
-    }
+    if (flashcards.length > 4) moveBy += randomizer;
+
     flashcards.splice(moveBy, 0, wasFirst);
   }
   function shuffleEasy(flashcards) {
     const wasFirst = flashcards.shift();
     let moveBy = flashcards.length - Math.floor(flashcards.length / 3);
     let randomizer = randomIntFromInterval(0, 3);
-    if (flashcards.length > 4) {
-      moveBy += randomizer;
-    }
+    if (flashcards.length > 4) moveBy += randomizer;
+
     flashcards.splice(moveBy, 0, wasFirst);
   }
   function shuffleCard() {
-    if (difficulty === 3) {
-      shuffleHard(flashcards);
-    }
-    if (difficulty === 2) {
-      shuffleMedium(flashcards);
-    }
-    if (difficulty === 1) {
-      shuffleEasy(flashcards);
-    }
+    if (difficulty === 3) shuffleHard(flashcards);
+
+    if (difficulty === 2) shuffleMedium(flashcards);
+
+    if (difficulty === 1) shuffleEasy(flashcards);
+
     if (difficulty === 0 && flashcards.length) {
       const passCard = flashcards.shift();
-      setCompletedFlashcards([...completedFlashcards, passCard]);
+      // setCompletedFlashcards([...completedFlashcards, passCard]);
     }
-    let updated = cloneDeep(flashcards);
-    setFlashcards(updated);
+    let flashcardsUpdate = cloneDeep(flashcards);
+    // setFlashcards(updated);
+    updateDeckFlashcards(flashcardsUpdate);
   }
   function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
   return (
     <>
       {flashcards.length > 0 ? (
