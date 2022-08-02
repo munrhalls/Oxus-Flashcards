9dbf8ee05593c7e22e0a2afd6ad600404bf9b987
John <antarcticdepths@gmail.com>
2022-08-02 12:35:01 +0200

    refactor!!! COMPLETED REFACTOR!!! OF EDIT DECK AND EDIT DECK LIST!!!

--- src/Modals/EditDeck/EditDeck.js
+++ src/Modals/EditDeck/EditDeck.js
@@ -1,49 +1,29 @@
 import React, { useState } from "react";
-import { Flashcard } from "../../Flashcards/Flashcard";
 import { uuidv4 } from "@firebase/util";
 import IMG__EDIT from "./../../Assets/edit.png";
 import IMG__CLOSE from "./../../Assets/close.png";
 import IMG__PLUS from "./../../Assets/plus.png";
 import IMG__SAVE from "./../../Assets/save.png";
-
-import cloneDeep from "lodash.clonedeep";
+import { EditFlashcardsList } from "./EditFlashcardsList";
 
 export const EditDeck = ({ closeModal, activeDeckId, decks, setDecks }) => {
   const deck = decks.filter((instance) => instance.id === activeDeckId)[0];
+  const [editedDeck, setEditedDeck] = useState(deck);
   const [isEditDeckName, setIsEditDeckName] = useState(null);
-  const [deckName, setDeckName] = useState(() => deck.name);
-  const [isDelConfirmId, setisDelConfirmId] = useState(null);
-  const [editedCards, setEditedCards] = useState(() => deck.flashcards);
-  const [editedCompletedCards, setEditedCompletedCards] = useState(
-    () => deck.completedFlashcards
-  );
 
   function handleSubmit(e) {
     e.preventDefault();
     setDecks(() =>
       decks.map((el) => {
-        return el.id === deck.id
-          ? {
-              ...deck,
-              name: deckName,
-              flashcards: editedCards,
-              completedFlashcards: editedCompletedCards,
-            }
-          : el;
+        return el.id === deck.id ? { ...editedDeck } : el;
       })
     );
     closeModal();
   }
-  function deleteCard(delId) {
-    setEditedCards((editedCards) =>
-      editedCards.filter((card) => card.id !== delId)
-    );
-    setEditedCompletedCards((editedCompletedCards) =>
-      editedCompletedCards.filter((card) => card.id !== delId)
-    );
-  }
+
   return (
     <div className="EditDeck">
+      <button>T E S T</button>
       <form className="Form" onSubmit={handleSubmit}>
         {/* <AddFlashcard /> */}
         <div className="Form__topBar">
@@ -56,11 +36,15 @@ export const EditDeck = ({ closeModal, activeDeckId, decks, setDecks }) => {
                 className="Form__topBar__line__deckName --input"
                 placeholder="type new deck name..."
                 type="text"
-                value={deckName}
-                onChange={(e) => setDeckName(e.target.value)}
+                value={editedDeck.name}
+                onChange={(e) =>
+                  setEditedDeck({ ...editedDeck, name: e.target.value })
+                }
               />
             ) : (
-              <h1 className="Form__topBar__line__deckName">{deckName}</h1>
+              <h1 className="Form__topBar__line__deckName">
+                {editedDeck.name}
+              </h1>
             )}
             {!isEditDeckName ? (
               <button
@@ -91,92 +75,10 @@ export const EditDeck = ({ closeModal, activeDeckId, decks, setDecks }) => {
             )}
           </div>
         </div>
-        <div className="Form__manageflashcards">
-          <div className="Form__manageflashcards__list">
-            {[...editedCards, ...editedCompletedCards].map((card, i) => {
-              return (
-                <div key={uuidv4()} className="Form__manageflashcardContainer">
-                  <div className="Form__manageflashcardContainer__numContainer">
-                    <div className="Form__manageflashcardContainer__numContainer__num">
-                      {i}
-                    </div>
-                  </div>
-
-                  {isDelConfirmId && isDelConfirmId === card.id ? (
-                    <div className="Form__manageflashcardContainer__deleteContainer">
-                      <h3 className="Form__manageflashcardContainer__deleteContainer__question">
-                        Are you sure?
-                      </h3>
-                      <button
-                        type="button"
-                        onClick={() => deleteCard(card.id)}
-                        className="Form__manageflashcardContainer__deleteContainer__btn --delete"
-                      >
-                        DELETE
-                      </button>
-                      <button
-                        type="button"
-                        onClick={() => setisDelConfirmId(null)}
-                        className="Form__manageflashcardContainer__deleteContainer__btn --back"
-                      >
-                        BACK
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="Form__manageflashcardContainer__elements">
-                      <Flashcard key={uuidv4()} flashcard={card} />
-                      <div className="Form__manageflashcardContainer__elements__btns">
-                        <button
-                          type="button"
-                          className="Form__manageflashcardContainer__elements__btns__btn --edit"
-                        >
-                          <img
-                            className="Form__manageflashcardContainer__elements__btns__btn__img"
-                            src={IMG__EDIT}
-                            alt="EDIT"
-                          />
-                        </button>
-
-                        <button
-                          type="button"
-                          onClick={() => setisDelConfirmId(card.id)}
-                          className="Form__manageflashcardContainer__elements__btns__btn --delete"
-                        >
-                          <img
-                            className="Form__manageflashcardContainer__elements__btns__btn__img"
-                            src={IMG__CLOSE}
-                            alt="DELETE"
-                          />
-                        </button>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              );
-            })}
-            <div className="Form__manageflashcardContainer --addBtn">
-              <button
-                type="button"
-                className="Form__manageflashcardContainer__btn --addBtn"
-              >
-                <img
-                  className="Form__manageflashcardContainer__btn__addImg"
-                  src={IMG__PLUS}
-                  alt="ADD"
-                />
-              </button>
-              <div className="Form__manageflashcardContainer__addBtnTextContainer">
-                <h6 className="Form__manageflashcardContainer__addBtnTextContainer__addBtnText">
-                  ADD NEW FLASHCARD
-                </h6>
-              </div>
-            </div>
-          </div>
-        </div>
-        {/* <div>
-            Add flashcard
-          </div> */}
-
+        <EditFlashcardsList
+          editedDeck={editedDeck}
+          setEditedDeck={(editedDeck) => setEditedDeck(editedDeck)}
+        />
         <div className="Form__exitBtnsContainer">
           <button
             type="button"
