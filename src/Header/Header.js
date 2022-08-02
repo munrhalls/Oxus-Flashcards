05dd88d2f2ceac9ff756d612b15af54f943914af
John <antarcticdepths@gmail.com>
2022-08-02 12:34:50 +0200

    header refactor 2

---
+++ src/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+export function Header({}) {
+  return <header className="Header">
+        Flashcards
+        <h6>&nbsp;&nbsp;&nbsp; /// PROTOTYPE: APP IN CONSTRUCTION.</h6>
+      </header>;
+}
+  
