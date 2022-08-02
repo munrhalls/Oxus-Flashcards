c3976e4eabe83f09650b70c361c536d9176f27bf
John <antarcticdepths@gmail.com>
2022-08-02 12:33:43 +0200

    modal absolute relative

--- src/Modals/Modals.js
+++ src/Modals/Modals.js
@@ -1,10 +1,17 @@
 import React from "react";
-import { AddFlashcard } from "../Flashcard/AddFlashcard";
+import { AddFlashcard } from "./AddFlashcard";
 
-export const Modals = ({set}) => {
+export const Modals = ({ setFlashcards, modalOpen }) => {
   return (
-    <div>
-      <AddFlashcard />
+    <div className="Modals">
+      {modalOpen === "AddFlashcard" ? (
+        <AddFlashcard setFlashcards={setFlashcards} />
+      ) : (
+        ""
+      )}
+      {modalOpen === "EditFlashcard" ? <div>EDIT MODAL</div> : ""}
+      {modalOpen === "DeleteFlashcard" ? <div>DELETE MODAL</div> : ""}
+      {modalOpen === "SortFlashcards" ? <div>SORT MODAL</div> : ""}
     </div>
   );
 };
