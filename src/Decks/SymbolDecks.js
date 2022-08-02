51159da28086ed33611c70c82501570a080d93e4
John <antarcticdepths@gmail.com>
2022-08-02 12:34:18 +0200

    big btn when 0 flashcards + msg

--- src/Decks/SymbolDecks.js
+++ src/Decks/SymbolDecks.js
@@ -3,11 +3,25 @@ import { SymbolDeck } from "./SymbolDeck";
 import { uuidv4 } from "@firebase/util";
 
 export const SymbolDecks = ({ decks }) => {
+  decks = [];
   return (
     <div className="SymbolDecks">
-      {decks.map((deck) => {
-        return <SymbolDeck key={uuidv4()} deck={deck} />;
-      })}
+      {decks.length ? (
+        decks.map((deck) => {
+          return <SymbolDeck key={uuidv4()} deck={deck} />;
+        })
+      ) : (
+        <div className="SymbolDecks__noDecks">
+          <div clasName="SymbolDecks__noDecksMsg">
+            <h1 className="SymbolDecks__noDecksMsg">
+              Flashcards repository empty.
+            </h1>
+          </div>
+          <button className="SymbolDecks__noDecksMsg__addBtn">
+            ADD NEW DECK
+          </button>
+        </div>
+      )}
     </div>
   );
 };
