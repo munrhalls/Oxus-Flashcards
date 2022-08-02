8b984cd325713e72137c3e78d774ca83a1b245ab
John <antarcticdepths@gmail.com>
2022-08-02 12:33:17 +0200

    turning fcards

--- src/Flashcard/Flashcard.js
+++ src/Flashcard/Flashcard.js
@@ -1,19 +1,18 @@
 import React, { useState } from "react";
 
-export const Flashcard = () => {
+export const Flashcard = ({ fcard }) => {
   const [turned, setTurned] = useState(false);
 
   function turn() {
-    console.log(turned);
     setTurned((turned) => !turned);
   }
 
   return (
     <div className="Flashcard" onClick={() => turn()}>
       {turned ? (
-        <div className="Flashcard__turned">TURNED</div>
+        <div className="Flashcard__turned">{fcard.turned}</div>
       ) : (
-        <div className="Flashcard__unturned">UNTURNED</div>
+        <div className="Flashcard__unturned">{fcard.unturned}</div>
       )}
     </div>
   );
