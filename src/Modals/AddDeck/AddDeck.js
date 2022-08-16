291c65e993c4d343ffc7027be14375c88b3132cb
John <antarcticdepths@gmail.com>
2022-08-16 11:54:36 +0200

    refactor edit fcard

--- src/Modals/AddDeck/AddDeck.js
+++ src/Modals/AddDeck/AddDeck.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { uuidv4 } from "@firebase/util";
+import { Form } from "../Form";
 import IMG__EDIT from "./../../Assets/edit.png";
 import IMG__CLOSE from "./../../Assets/close.png";
 import IMG__SAVE from "./../../Assets/save.png";
@@ -45,28 +46,7 @@ export const AddDeck = ({ decks, setDecks, setModalOpen, setactiveDeckId }) => {
           />
         </div>
 
-        <div className="Form__exitBtnsContainer">
-          <button
-            type="button"
-            className="Form__exitBtnsContainer__btn"
-            onClick={() => setModalOpen(null)}
-          >
-            <span className="Form__exitBtnsContainer__btn__text">Close</span>
-            <img
-              className="Form__exitBtnsContainer__btn__img"
-              src={IMG__CLOSE}
-              alt="CLOSE"
-            />
-          </button>
-          <button className="Form__exitBtnsContainer__btn" type="submit">
-            <span className="Form__exitBtnsContainer__btn__text">Save</span>
-            <img
-              className="Form__exitBtnsContainer__btn__img"
-              src={IMG__SAVE}
-              alt="SAVE"
-            />
-          </button>
-        </div>
+        <Form.SubmitExitBtns setModalOpen={() => setModalOpen(null)} />
       </form>
     </div>
   );
