4135a81155632b48cf4cad0e0182123560b40393
John <antarcticdepths@gmail.com>
2022-08-17 17:26:17 +0200

    reset btn style refactor

--- src/Deck/ResetDeckBtn.js
+++ src/Deck/ResetDeckBtn.js
@@ -1,15 +1,13 @@
 import React from "react";
+import IMG__RESET from "./../Assets/go-back-arrow.png";
 
 export default function ResetDeckBtn({ resetDeck }) {
   return (
-    <div className="Flashcards__completedMessage__resetDeck">
-      <button
-        className="Flashcards__completedMessage__resetDeck__btn"
-        onClick={() => resetDeck()}
-      >
+    <div className="Deck__resetDeck">
+      <button className="Deck__resetDeck__btn" onClick={() => resetDeck()}>
         RESET
       </button>
-      <img className="Flashcards__completedMessage__resetDeck__img" />
+      <img src={IMG__RESET} className="Deck__resetDeck__img" />
     </div>
   );
 }
