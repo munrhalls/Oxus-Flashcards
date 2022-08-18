e71597f5e751d80c80b30cf4ff0d04c79075f52c
John <antarcticdepths@gmail.com>
2022-08-18 17:05:44 +0200

    user log in register ue ui

--- src/ModalsUser/Register/Register.js
+++ src/ModalsUser/Register/Register.js
@@ -2,9 +2,15 @@ import React from "react";
 import { FormUser } from "./../FormUser";
 
 export default function Register() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit");
+  }
   return (
-    <div className="Register">
-      <div className="Register__topbar">topbar title</div>
+    <form className="Register">
+      <div className="Register__topbar">
+        <h1 className="Register__topbar__title">REGISTER</h1>
+      </div>
       <div className="Register__inputs">
         <label className="Register__inputs__label">E-mail address:</label>
         <input className="Register__inputs__email"></input>
@@ -14,8 +20,10 @@ export default function Register() {
       {/* reset pw */}
       <div className="Register__exit">
         <button className="Register__exit__cancel">cancel btn</button>
-        <button className="Register__exit__submit">submit btn</button>
+        <button className="Register__exit__submit" onClick={handleSubmit}>
+          submit btn
+        </button>
       </div>
-    </div>
+    </form>
   );
 }
