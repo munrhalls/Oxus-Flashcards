631bad58e7df7488f0d776a1be677c3aa1568ea3
John <antarcticdepths@gmail.com>
2022-08-18 07:53:43 +0200

    user log in register ue ui

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,8 +4,8 @@ export function Header({}) {
     <header className="Header">
       <h6 className="Header__title">Flashcards</h6>
       <div className="Header__account">
-        <button className="Header__account__logIn">Log in</button>
-        <button className="Header__account__register">Register</button>
+        <button className="Header__account__btn">Log in</button>
+        <button className="Header__account__btn">Register</button>
       </div>
     </header>
   );
