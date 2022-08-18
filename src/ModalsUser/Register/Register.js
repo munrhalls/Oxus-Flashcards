a06239a5fe124ff338476b2d092530d5c962c2c4
John <antarcticdepths@gmail.com>
2022-08-18 14:25:31 +0200

    user log in register ue ui

--- src/ModalsUser/Register/Register.js
+++ src/ModalsUser/Register/Register.js
@@ -3,14 +3,18 @@ import { FormUser } from "./../FormUser";
 
 export default function Register() {
   return (
-    <div>
-      <div>topbar title</div>
-      <div>register user input</div>
-      <div>register password input</div>
+    <div className="Register">
+      <div className="Register__topbar">topbar title</div>
+      <div className="Register__inputs">
+        <div className="Register__inputs__email">register email input</div>
+        <div className="Register__inputs__password">
+          register password input
+        </div>
+      </div>
       {/* reset pw */}
-      <div>
-        <button>cancel btn</button>
-        <button>submit btn</button>
+      <div className="Register__exit">
+        <button className="Register__exit__cancel">cancel btn</button>
+        <button className="Register__exit__submit">submit btn</button>
       </div>
     </div>
   );
