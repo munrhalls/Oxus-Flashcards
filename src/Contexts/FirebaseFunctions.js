16559858ed38322a51a1eb7e78a2df7bbdaea8d6
John <antarcticdepths@gmail.com>
2022-08-25 15:29:46 +0200

    before rework

---
+++ src/Contexts/FirebaseFunctions.js
@@ -0,0 +1,62 @@
+import firebase from "../Firebase";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
+
+export default function FirebaseFunctions() {
+  const [currentUser, setCurrentUser] = useState();
+
+  function getCurrentUser() {
+    return currentUser;
+  }
+
+  function register(email, password) {
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  }
+
+  function login(email, password) {
+    const auth = getAuth();
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
+  const value = {
+    getCurrentUser,
+    register,
+    login,
+    logout,
+  };
+  return value;
+}
--- src/Contexts/GlobalProvider.js
+++ src/Contexts/GlobalProvider.js
@@ -7,6 +7,7 @@ import {
   signOut,
 } from "firebase/auth";
 import React, { useContext, useEffect, useState } from "react";
+import FirebaseFunctions from "./FirebaseFunctions";
 
 const Global = React.createContext();
 export function useGlobal() {
@@ -14,55 +15,57 @@ export function useGlobal() {
 }
 
 export function GlobalProvider({ children }) {
-  const [currentUser, setCurrentUser] = useState();
-  console.log(currentUser?.email, " in FirebaseProvider");
+  // const [currentUser, setCurrentUser] = useState();
 
-  function getCurrentUser() {
-    return currentUser;
-  }
+  // function getCurrentUser() {
+  //   return currentUser;
+  // }
 
-  function register(email, password) {
-    const auth = getAuth();
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        console.log(errorMessage);
-      });
-  }
+  // function register(email, password) {
+  //   const auth = getAuth();
+  //   createUserWithEmailAndPassword(auth, email, password)
+  //     .then((userCredential) => {
+  //       // Signed in
+  //       const user = userCredential.user;
+  //     })
+  //     .catch((error) => {
+  //       const errorCode = error.code;
+  //       const errorMessage = error.message;
+  //       console.log(errorMessage);
+  //     });
+  // }
 
-  function login(email, password) {
-    const auth = getAuth();
-    return signInWithEmailAndPassword(auth, email, password);
-  }
+  // function login(email, password) {
+  //   const auth = getAuth();
+  //   return signInWithEmailAndPassword(auth, email, password);
+  // }
 
-  function logout() {
-    const auth = getAuth();
-    signOut(auth)
-      .then(() => {
-        // Sign-out successful.
-      })
-      .catch((error) => {
-        // An error happened.
-      });
-  }
+  // function logout() {
+  //   const auth = getAuth();
+  //   signOut(auth)
+  //     .then(() => {
+  //       // Sign-out successful.
+  //     })
+  //     .catch((error) => {
+  //       // An error happened.
+  //     });
+  // }
 
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
-    });
-  }, []);
+  // useEffect(() => {
+  //   const auth = getAuth();
+  //   onAuthStateChanged(auth, (user) => {
+  //     setCurrentUser(user);
+  //   });
+  // }, []);
 
-  const value = {
-    getCurrentUser,
-    register,
-    login,
-    logout,
-  };
+  // const value = {
+  //   getCurrentUser,
+  //   register,
+  //   login,
+  //   logout,
+  // };
+  const value = FirebaseFunctions();
+  console.log(value);
+
   return <Global.Provider value={value}>{children}</Global.Provider>;
 }
