f65e97237b704efe0c7f4b42e854d134a919ae78
John <antarcticdepths@gmail.com>
2022-08-26 15:37:26 +0200

    loader

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,28 +1,36 @@
 import { useGlobal } from "../../Contexts/GlobalProvider";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { FormUser } from "./../ModalsUser/FormUser";
+import Loader from "../Loader/Loader";
 
 export function Header() {
   const { getCurrentUser } = useGlobal();
+  const [isLoading, setIsLoading] = useState(false);
   const currentUser = getCurrentUser();
+
+  useEffect(() => {
+    setIsLoading(true);
+  }, [currentUser]);
 
   return (
     <header className="Header">
       <h6 className="Header__title">Flashcards</h6>
       <div className="Header__account">
-        {currentUser?.email ? (
-          <>
-            <h1 className="Header__account__welcomeMsg">
-              Welcome {currentUser?.email} !
-            </h1>
-            <FormUser.LogOutBtn />
-          </>
-        ) : (
-          <>
-            <FormUser.LoginBtn />
-            <FormUser.RegisterBtn />
-          </>
-        )}
+        <Loader active={isLoading}>
+          {currentUser?.email ? (
+            <>
+              <h1 className="Header__account__welcomeMsg">
+                Welcome {currentUser?.email} !
+              </h1>
+              <FormUser.LogOutBtn />
+            </>
+          ) : (
+            <>
+              <FormUser.LoginBtn />
+              <FormUser.RegisterBtn />
+            </>
+          )}
+        </Loader>
       </div>
     </header>
   );
