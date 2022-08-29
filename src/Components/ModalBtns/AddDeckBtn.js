f86fa2432253f48efc3fa34315862052af069fb0
John <antarcticdepths@gmail.com>
2022-08-29 12:57:36 +0200

    cards effect

--- src/Components/ModalBtns/AddDeckBtn.js
+++ src/Components/ModalBtns/AddDeckBtn.js
@@ -2,23 +2,39 @@ import React from "react";
 import IMG__PLUS from "./../../Assets/plus.png";
 import IMG__CARDS from "./../../Assets/cards.png";
 import { useGlobal } from "../../Contexts/GlobalProvider";
+import { uuidv4 } from "@firebase/util";
 
 export const AddDeckBtn = () => {
   const { setModalOpen } = useGlobal();
-  
+
   return (
     <div onClick={() => setModalOpen("AddDeck")} className="AddDeckBtn">
       <button className="AddDeckBtn__btn">ADD NEW DECK</button>
+      {/* map 9 imgs */}
+      {/* begin kind of far left horiz, center vert */}
+      {/* on every iteration, place next item diagonally to the top right */}
+      {/* enable alteration every 3 iterations */}
+      {/* every alteration, transform size to bigger symmetrically & more top-left away from center */}
+      {/* on hover, these will be moving /w keyframes */}
       <img
         className="AddDeckBtn__imgPlus"
         src={IMG__PLUS}
         alt="ADD BUTTON IMAGE."
       />
-      <img
-        className="AddDeckBtn__imgCards"
-        src={IMG__CARDS}
-        alt="CARDS IMAGE."
-      />
+
+      {Array(4)
+        .fill()
+        .map((icon, i) => {
+          return (
+            <img
+              style={{ left: `${2 * i}rem`, transform: `rotate(-30deg)` }}
+              key={uuidv4()}
+              className="AddDeckBtn__imgCards"
+              src={IMG__CARDS}
+              alt="CARDS IMAGE."
+            />
+          );
+        })}
     </div>
   );
 };
