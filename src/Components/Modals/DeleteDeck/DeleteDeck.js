b5fc41a9ff7b3083baf9de476e481f7418e8ae5c
John <antarcticdepths@gmail.com>
2022-09-02 15:08:01 +0200

    del deck method

--- src/Components/Modals/DeleteDeck/DeleteDeck.js
+++ src/Components/Modals/DeleteDeck/DeleteDeck.js
@@ -3,20 +3,41 @@ import { Form } from "../Form";
 import { useGlobal } from "../../../Contexts/GlobalProvider";
 
 export const DeleteDeck = (props) => {
-  const { modalOpen, setModalOpen, decks, setDecks } = useGlobal();
-  
+  const {
+    DB_deleteDeck,
+    getDecksFromDBAndUpdateUI,
+    currentUser,
+    modalOpen,
+    setModalOpen,
+    decks,
+    setDecks,
+  } = useGlobal();
+
   const { setActiveDeckId, activeDeckId } = props;
   const deck = decks.filter((instance) => instance.id === activeDeckId)[0];
 
-  function handleSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
-    setModalOpen(null);
-    setActiveDeckId(null);
-    setDecks(() =>
-      [...decks].filter((el) => {
-        return el.id !== activeDeckId;
-      })
-    );
+
+    if (!currentUser)
+      return (function () {
+        setActiveDeckId(null);
+        setDecks(() =>
+          [...decks].filter((el) => {
+            return el.id !== activeDeckId;
+          })
+        );
+        setModalOpen(null);
+      })();
+
+    try {
+      await DB_deleteDeck(currentUser, activeDeckId);
+      await getDecksFromDBAndUpdateUI(currentUser);
+      setActiveDeckId(null);
+      setModalOpen(null);
+    } catch (e) {
+      console.error(e);
+    }
   }
   return (
     <div>
