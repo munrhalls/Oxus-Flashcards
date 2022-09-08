e18e99a202cf5d6f81d13a674bc1b24bccbe8554
John <antarcticdepths@gmail.com>
2022-09-08 18:40:26 +0200

    welcome msg pos

--- src/Components/Welcome/Welcome.js
+++ src/Components/Welcome/Welcome.js
@@ -30,74 +30,78 @@ export const Welcome = {
     useEffect(() => {
       setTimeout(() => {
         setIsLoading(false);
-      }, 1500);
+      }, 1250);
     });
 
     return (
-      <Loader active={isLoading}>
+      <>
         {message ? (
           <div className="WelcomeMessage">
-            <h1 className="WelcomeMessage__title">Welcome to flashcards!</h1>
-            <h2 className="WelcomeMessage__subtitle --link">
-              <a href="https://en.wikipedia.org/wiki/Flashcard" target="_blank">
-                What are flashcards?&nbsp;
-              </a>
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              Flashcards are a great tool for learning. This app lets you easily
-              create and review decks of flashcards.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              Flashcards work like this: you write question on one side, and
-              answer on the other side. You make at least a few such cards, like
-              a deck of cards.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              Then you flip through them, one by one. Each time, you try to
-              answer the question on your own. Then you check it with the answer
-              you've written on the other side.
-            </h2>
-
-            <h2 className="WelcomeMessage__subtitle">
-              If the card was hard, you put it at the start of the deck. If
-              medium, in the middle. If easy, at the end of the deck. If you've
-              repeated a given card so many times you think you've completely
-              gotten it, you put that card away.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              You continue like this, until you've completed (learned) all
-              flashcards. Each flashcard represents a small portion of
-              information that it gets you to{" "}
-              <span className="--italic">
-                easily learn by providing automated{" "}
-                <a
-                  href="https://en.wikipedia.org/wiki/Spaced_repetition"
-                  target="_blank"
-                >
-                  spaced-repetition
-                </a>
-                .
-              </span>
-            </h2>
-
-            <h2 className="WelcomeMessage__subtitle">
-              It's an electronic version of the well-known, popular,{" "}
-              <span className="--italic">
-                simple and highly effective learning tool
-              </span>
-              : flashcards.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle --link">
-              <div>
+            <Loader active={isLoading}>
+              <h1 className="WelcomeMessage__title">Welcome to flashcards!</h1>
+              <h2 className="WelcomeMessage__subtitle --link">
                 <a
                   href="https://en.wikipedia.org/wiki/Flashcard"
                   target="_blank"
                 >
-                  Click to read Wikipedia article.
+                  What are flashcards?&nbsp;
                 </a>
-              </div>
-            </h2>
-            {/* <ul className="WelcomeMessage__list">
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                Flashcards are a great tool for learning. This app lets you
+                easily create and review decks of flashcards.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                Flashcards work like this: you write question on one side, and
+                answer on the other side. You make at least a few such cards,
+                like a deck of cards.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                Then you flip through them, one by one. Each time, you try to
+                answer the question on your own. Then you check it with the
+                answer you've written on the other side.
+              </h2>
+
+              <h2 className="WelcomeMessage__subtitle">
+                If the card was hard, you put it at the start of the deck. If
+                medium, in the middle. If easy, at the end of the deck. If
+                you've repeated a given card so many times you think you've
+                completely gotten it, you put that card away.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                You continue like this, until you've completed (learned) all
+                flashcards. Each flashcard represents a small portion of
+                information that it gets you to{" "}
+                <span className="--italic">
+                  easily learn by providing automated{" "}
+                  <a
+                    href="https://en.wikipedia.org/wiki/Spaced_repetition"
+                    target="_blank"
+                  >
+                    spaced-repetition
+                  </a>
+                  .
+                </span>
+              </h2>
+
+              <h2 className="WelcomeMessage__subtitle">
+                It's an electronic version of the well-known, popular,{" "}
+                <span className="--italic">
+                  simple and highly effective learning tool
+                </span>
+                : flashcards.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle --link">
+                <div>
+                  <a
+                    href="https://en.wikipedia.org/wiki/Flashcard"
+                    target="_blank"
+                  >
+                    Click to read Wikipedia article.
+                  </a>
+                </div>
+              </h2>
+              {/* <ul className="WelcomeMessage__list">
               <li className="WelcomeMessage__list__item">
                 To start, first click the "ADD NEW DECK" button.
               </li>
@@ -119,7 +123,7 @@ export const Welcome = {
                 can add and edit deck's flashcards.
               </li>
             </ul> */}
-            {/* <h2 className="WelcomeMessage__subtitle">
+              {/* <h2 className="WelcomeMessage__subtitle">
               Add as many flashcards as you want to a deck. You can also create
               as many different decks with different flashcards as you want.
             </h2>
@@ -127,39 +131,40 @@ export const Welcome = {
               To turn a flashcard, click it. To go to the next flashcard, mark
               how hard the flashcard was, and click "NEXT".
             </h2> */}
-            <h2 className="WelcomeMessage__subtitle">
-              This app is designed to offer simple, practical, no-nonsense
-              solution for making and using electronic flashcards.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              You can easily add pure text or{" "}
-              <span className="--bold --italic">images</span>, or both{" "}
-              <span className="--bold --italic">text and images</span>, in your
-              flashcards.
-            </h2>
-            <h2 className="WelcomeMessage__subtitle">
-              For mobile phone users, you can easily{" "}
-              <span className="--bold --italic">
-                take a picture with phone camera and upload it to your flashcard
-                in 1 step!
-              </span>
-            </h2>
-            <h2 className="WelcomeMessage__subtitle --fontLarge">
-              Enjoy Oxus Flashcards!
-            </h2>
-            <div
-              onClick={() => setIsMessage((message) => false)}
-              className="WelcomeMessage__btnContainer"
-            >
-              <button className="WelcomeMessage__btnContainer__btn">
-                Close this message
-              </button>
-            </div>
+              <h2 className="WelcomeMessage__subtitle">
+                This app is designed to offer simple, practical, no-nonsense
+                solution for making and using electronic flashcards.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                You can easily add pure text or{" "}
+                <span className="--bold --italic">images</span>, or both{" "}
+                <span className="--bold --italic">text and images</span>, in
+                your flashcards.
+              </h2>
+              <h2 className="WelcomeMessage__subtitle">
+                For mobile phone users, you can easily{" "}
+                <span className="--bold --italic">
+                  take a picture with phone camera and upload it to your
+                  flashcard in 1 step!
+                </span>
+              </h2>
+              <h2 className="WelcomeMessage__subtitle --fontLarge">
+                Enjoy Oxus Flashcards!
+              </h2>
+              <div
+                onClick={() => setIsMessage((message) => false)}
+                className="WelcomeMessage__btnContainer"
+              >
+                <button className="WelcomeMessage__btnContainer__btn">
+                  Close this message
+                </button>
+              </div>
+            </Loader>
           </div>
         ) : (
           ""
         )}
-      </Loader>
+      </>
     );
   },
 };
