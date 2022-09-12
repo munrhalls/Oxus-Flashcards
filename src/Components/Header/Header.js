98a210a2f8967469d9068853f5d613e752e9cbb4
John <antarcticdepths@gmail.com>
2022-09-12 15:00:16 +0200

    about btn works

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,3 +1,4 @@
+import { uuidv4 } from "@firebase/util";
 import { useGlobal } from "../../Contexts/GlobalProvider";
 import React, { useState, useEffect } from "react";
 import { FormUser } from "../FormUser/FormUser";
@@ -33,6 +34,29 @@ export function Header({ setActiveDeckId }) {
       </h6>
 
       <h6 className="Header__title">
+        <div className="Header__Header__title__decorIconsContainer">
+          {Array(16)
+            .fill()
+            .map((icon, i) => {
+              i++;
+              return (
+                <img
+                  style={{
+                    bottom: `${-0.25 + i * 1.2}rem`,
+                    left: `${-4.5 + 0.25 * i * 4.25 + (1 / i) * 5}rem`,
+                    boxShadow: `0 0 15px 5px rgb(${255}, ${255}, ${255})`,
+                    borderRadius: `50%`,
+                    padding: `0.25rem`,
+                    transform: `rotateY(${i * 2.5}deg) scale(${1 - i / 12.5})`,
+                  }}
+                  key={uuidv4()}
+                  className="Header__Header__title__decorIconsContainer__decorIcons"
+                  src={IMG__CARDS}
+                  alt="CARDS IMAGE."
+                />
+              );
+            })}
+        </div>
         <img
           className="Header__title__icon"
           src={IMG__CARDS}
