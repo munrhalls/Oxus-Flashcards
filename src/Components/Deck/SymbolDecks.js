6d752d6db21d73762f232da1e3f357c6ffd3ce40
John <antarcticdepths@gmail.com>
2022-09-12 11:02:08 +0200

    title

--- src/Components/Deck/SymbolDecks.js
+++ src/Components/Deck/SymbolDecks.js
@@ -9,27 +9,31 @@ export const SymbolDecks = ({ decks, setActiveDeckId }) => {
   const { setModalOpen } = useGlobal();
 
   return (
-    <div className="SymbolDecks">
-      <AddDeckBtn />
-      {decks.length ? (
-        decks.map((deck) => {
-          return (
-            <SymbolDeck
-              key={uuidv4()}
-              deck={deck}
-              setActiveDeckId={setActiveDeckId}
-            />
-          );
-        })
-      ) : (
-        <div className="SymbolDecks__noDecks">
-          <div className="SymbolDecks__noDecksMsg">
-            <h1 className="SymbolDecks__noDecksMsg">
-              Decks repository is empty.
-            </h1>
+    <>
+      <h1 className="SymbolDecks__title">List of decks</h1>
+
+      <div className="SymbolDecks">
+        <AddDeckBtn />
+        {decks.length ? (
+          decks.map((deck) => {
+            return (
+              <SymbolDeck
+                key={uuidv4()}
+                deck={deck}
+                setActiveDeckId={setActiveDeckId}
+              />
+            );
+          })
+        ) : (
+          <div className="SymbolDecks__noDecks">
+            <div className="SymbolDecks__noDecksMsg">
+              <h1 className="SymbolDecks__noDecksMsg">
+                Decks repository is empty.
+              </h1>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </>
   );
 };
